2911743ec3d35802f85b1786898a2b69ada6d08b
Jihan Haya Mufialdo <92241700+JihanHayaMufialdo@users.noreply.github.com>
2021-11-09 16:14:50 +0700

    Add files via upload

---
+++ 2117051095_JihanHayaMufialdo_Pretest1.cpp
@@ -0,0 +1,35 @@
+#include <iostream>
+using namespace std;
+int main(){
+
+	//title
+	cout<<"DATA PELANGGAN SEMBAKO"<<endl;
+	cout<<""<<endl;
+	
+	/* 
+	   Jihan Haya Mufialdo
+	   Jl. Hangtuah IV No. 19, Pekanbaru
+	   jihannhayaa@gmail.com
+	   082268210439 
+	*/
+    
+    //string
+	string namapelanggan;
+	string alamatrumah;
+	string emailpelanggan;
+	int nomortelepon;
+	
+	getline (cin,namapelanggan);
+	getline (cin,alamatrumah);
+    cin>>emailpelanggan;
+	cin>>nomortelepon;
+	cout<<""<<endl;
+	
+	//output
+	cout<<"Nama Pelanggan    :   "<<namapelanggan<<endl;
+	cout<<"Alamat Rumah      :   "<<alamatrumah<<endl;
+	cout<<"Email Pelanggan   :   "<<emailpelanggan<<endl;
+	cout<<"Nomor Telepon     :   "<<nomortelepon<<endl;
+	
+	return 0;
+}
