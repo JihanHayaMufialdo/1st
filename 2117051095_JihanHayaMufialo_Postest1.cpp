3092a04cca805419084f78a7c138d6dad361adc3
Jihan Haya Mufialdo <92241700+JihanHayaMufialdo@users.noreply.github.com>
2021-11-09 12:13:13 +0700

    code

---
+++ 2117051095_JihanHayaMufialo_Postest1.cpp
@@ -0,0 +1,45 @@
+#include <iostream>
+using namespace std;
+int main (){
+	
+	/* JIHAN HAYA MUFIALDO
+	   2117051095
+	   KELAS B
+	*/
+	
+	cout << "--------------------------------MEMBUAT AKUN SIAKADU---------------------------------" << endl;
+	
+	cout << "" << endl;
+	
+	cout << "masukkan NPM, tanggal lahir, bulan lahir, dan tahun lahir anda dalam 4 baris beruntun" << endl;
+	
+	cout << "" << endl;
+	
+	int     NPM            ;
+	int     tanggalahir    ; 
+	string  bulanlahir     ;
+	int     tahunlahir     ;
+	
+	//input
+	cin  >> NPM         ;
+	cin  >> tanggalahir ;
+	cin  >> bulanlahir  ;
+	cin  >> tahunlahir  ;
+	 
+	cout << "" << endl;
+	
+	cout << "DATA AKUN SIAKADU ANDA :" << endl;
+	
+	cout << "" << endl;
+	
+	//output
+	cout << "USERNAME : " << NPM << endl;
+	cout << "PASSWORD : " << tahunlahir << bulanlahir << tanggalahir << endl;
+	
+	return 0;
+}
+
+
+
+
+
