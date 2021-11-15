6c0e9a7e89451636e130a2f0611056adaaa69992
Jihan Haya Mufialdo <92241700+JihanHayaMufialdo@users.noreply.github.com>
2021-11-15 23:57:59 +0700

    Add files via upload

---
+++ Input Output Kampus.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+using namespace std;
+
+int main(){
+	
+	string prodi, jurusan, fakultas, univ;
+	
+	cout << "Prodi : ";
+	cin >> prodi;
+	cout << "Jurusan : ";
+	cin >> jurusan;
+	cout << "Fakultas : ";
+	cin >> fakultas;
+	cout << "Universitas : ";
+	cin >> univ;
+	
+	return 0;
+}
