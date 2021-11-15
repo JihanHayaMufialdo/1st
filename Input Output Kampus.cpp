d32571029faecdce7ec3c80acd49af7c886cb1d4
Jihan Haya Mufialdo <92241700+JihanHayaMufialdo@users.noreply.github.com>
2021-11-16 00:02:56 +0700

    Update Input Output Kampus.cpp

--- Input Output Kampus.cpp
+++ Input Output Kampus.cpp
@@ -5,14 +5,19 @@ int main(){
 	
 	string prodi, jurusan, fakultas, univ;
 	
-	cout << "Prodi\t: ";
+	cout << "Prodi\t\t: ";
 	getline(cin,prodi);
-	cout << "Jurusan\t: ";
+	cout << "Jurusan\t\t: ";
 	getline(cin,jurusan);
 	cout << "Fakultas\t: ";
-	cin >> fakultas;
+	getline(cin,fakultas);
 	cout << "Universitas\t: ";
-	cin >> univ;
+	getline(cin,univ);
+	
+	cout << "Prodi\t\t: " << prodi;
+	cout << "Jurusan\t\t: " << jurusan;
+	cout << "Fakultas\t: " << fakultas;
+	cout << "Universitas\t: " << univ;
 	
 	return 0;
 }
